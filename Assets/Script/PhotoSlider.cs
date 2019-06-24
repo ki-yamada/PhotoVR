ff3a4177c21cda9f96fb715fbb965570f326154a
山田清貴 <kiyotakay96@gmail.com>
2019-06-24 15:23:29 +0900

    first commit

---
+++ Assets/Script/PhotoSlider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoSlider : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] _PhotoList;
+
+    [SerializeField]
+    private SpriteRenderer _Photo;
+    private int _Index;
+
+    private void Start()
+    {
+        _Index = 1;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangePhoto();
+        }
+    }
+
+    private void ChangePhoto()
+    {
+        if (_Index >= _PhotoList.Length)
+        {
+            _Index = 0;
+        }
+        _Photo.sprite = _PhotoList[_Index];
+        _Index++;
+
+    }
+
+}
